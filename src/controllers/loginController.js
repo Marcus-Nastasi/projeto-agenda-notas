3acc87957519062603856f8157299808bd9ebf36
rolemberg <vinnie.nsts@gmail.com>
2024-01-16 18:47:37 -0300

    changes on login Controller, redirect without flashes

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,9 @@
 const CreateUser = require('../models/CreateUser');
 
-exports.index = (req, res) => res.render('login', { sucess: false, err: false });
+var err = false;
+var sucess = false;
+
+exports.index = (req, res) => res.render('login', { sucess: sucess, err: err });
 
 // user controller
 exports.user = (req, res) => res.render('404');
@@ -10,19 +13,18 @@ exports.register = async (req, res) => {
    const registroLogin = new CreateUser(req.body);
    await registroLogin.registraUsuario();
 
-   var err = false;
-   var sucess = false;
-
    if(registroLogin.errors.length == 0) {
       err = false;
       sucess = true;
    } else {
       err = registroLogin.errors;
-      sucess = false
+      sucess = false;
    }
 
-   // res.redirect('back'); // volta para página anterior. 
+   req.session.save(function() {
+      res.redirect('back'); // volta para página anterior. 
+   })
 
-   res.render('login', { sucess: sucess, err: err });
+   // res.render('login', { sucess: sucess, err: err });
 };
 
